dbae1e754d8fb1d5eea1cfde890451121bd69f15
Anujpatel007 <patelanuj136@gmail.com>
2024-09-17 23:07:42 +0930

    final file of task 1

--- ass.2/que2_1.py
+++ ass.2/que2_1.py
@@ -16,17 +16,18 @@ image = Image.open('Chapter1.jpg')
 pixels = list(image.getdata())
 n=generated_number
 upgraded_pixels = [(r + n, g + n, b + n) for r, g, b in pixels]
-# want to creat the same thing with the g and b and compair the output as like (r + n, g + n, b + n) for r
-new_image = Image.new(image.mode, image.size)
-new_image.putdata(upgraded_pixels)
-new_image.save('chapter1out.jpg')
+upgraded_image= Image.new(image.mode, image.size)
+upgraded_image.putdata(upgraded_pixels)
+upgraded_image.save('chapter1out.jpg')
 
 # Calculate sum of red pixel values
 red_sum = sum([r for r, g, b in upgraded_pixels])
 print(f"Sum of red pixels: {red_sum}")
 
-green_sum = sum([ g for r, g, b in upgraded_pixels])
+# Calculate sum of green pixel values
+green_sum = sum([g for r, g, b in upgraded_pixels])
 print(f"Sum of green pixels: {green_sum}")
 
-blue_sum = sum([ b for r, g, b in upgraded_pixels])
+# Calculate sum of blue pixel values
+blue_sum = sum([b for r, g, b in upgraded_pixels])
 print(f"Sum of blue pixels: {green_sum}")
