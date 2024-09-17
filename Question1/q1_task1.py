c249fe88f662a0fce74ea9a434f6446b22502732
HIT137 <myemail@example.com>
2024-09-18 01:04:49 +0930

    Download NLTK tokenizer data

--- Question1/q1_task1.py
+++ Question1/q1_task1.py
@@ -5,3 +5,6 @@ from nltk.tokenize import word_tokenize
 # Download NLTK tokenizer data
 nltk.download('punkt')
 
+# List of CSV file paths
+csv_files = ['CSV1.csv', 'CSV2.csv', 'CSV3.csv', 'CSV4.csv']
+
