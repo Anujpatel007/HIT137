41060eb601db237d5e4b2f0cf33092b41acc07e5
HIT137 <myemail@example.com>
2024-09-18 07:09:23 +0930

    Iterate over each column

--- Question1/q1_task1.py
+++ Question1/q1_task1.py
@@ -11,3 +11,7 @@ csv_files = ['CSV1.csv', 'CSV2.csv', 'CSV3.csv', 'CSV4.csv']
 # Function to find the column with the largest text using NLTK
 def find_large_text_columns_nltk(df):
     text_lengths = {}
+    # Iterate over each column
+    for column in df.columns:
+        if df[column].dtype == object:
+            
