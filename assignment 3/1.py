e5e73f3301af1f5d0bfc213fbde0c73d61f392bd
Anujpatel007 <patelanuj136@gmail.com>
2024-10-16 18:30:39 +0930

    completed AI model

--- assignment 3/1.py
+++ assignment 3/1.py
@@ -58,3 +58,7 @@ class ImageModel:
         img_array = tf.expand_dims(img_array, axis=0)
         img_array = tf.keras.applications.mobilenet_v2.preprocess_input(img_array)
 
+        # Predict using the model
+        predictions = self.model.predict(img_array)
+        decoded_predictions = tf.keras.applications.mobilenet_v2.decode_predictions(predictions, top=1)[0]
+        return decoded_predictions[0][1]  # Return the predicted label
