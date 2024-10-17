4e0e588843d6f61b2742e93e09a098dc84e548a4
HIT137 <myemail@example.com>
2024-10-18 00:30:14 +0930

    screen dimensions

--- assignment 3/2.py
+++ assignment 3/2.py
@@ -3,3 +3,9 @@ import random
 from pygame.locals import (
     K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, K_SPACE, K_j, KEYDOWN, QUIT,
 )
+# Initialize pygame
+pygame.init()
+
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
